5c08321bf2f00b327cbb8882bb89b1866f6228f2
Nenad Pavlovic <nenadp@elfak.rs>
2024-08-24 15:31:47 +0200

    registration+firebase

---
+++ Client/src/firabse.jsx
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlaAoiEKuQf3NM_9ouFQlpq3xLqEjl7Yo",
+  authDomain: "recommendationsystem-ae8b5.firebaseapp.com",
+  projectId: "recommendationsystem-ae8b5",
+  storageBucket: "recommendationsystem-ae8b5.appspot.com",
+  messagingSenderId: "826303939239",
+  appId: "1:826303939239:web:c640c76adf2dcbe04f1182",
+};
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
--- Client/src/pages/Auth/Register.jsx
+++ Client/src/pages/Auth/Register.jsx
@@ -3,27 +3,85 @@ import { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../components/Loader";
+import "react-datepicker/dist/react-datepicker.css";
+import { useRegisterMutation } from "../../redux/api/usersApiSlice";
+import DatePicker from "react-datepicker";
 import { toast } from "react-toastify";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+import { storage } from "../../firabse";
 const Register = () => {
-  const [username, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [surname, setSurname] = useState("");
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [dateBirth, setDateBirth] = useState("");
+  const [address, setAddress] = useState("");
+  const [photo, setPhoto] = useState("");
   const { search } = useLocation();
   const sp = new URLSearchParams(search);
-  const redirect = sp.get("redirect") || "/";
-  const register = (e) => {};
+  const redirect = sp.get("redirect") || "/login";
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.auth);
+  const [register, { isLoading }] = useRegisterMutation();
+  useEffect(() => {
+    if (userInfo) navigate(redirect);
+  }, [navigate, redirect, userInfo]);
+  const registUser = async (e) => {
+    e.preventDefault();
+    if (password != confirmPassword) toast.error("Passwords do not match");
+    else {
+      try {
+        const imageRef = ref(storage, `users/${photo.name + v4()}`);
+        console.log("KURCINA2");
+        uploadBytes(imageRef, photo).then(() => {
+          getDownloadURL(imageRef).then(async (res) => {
+            try {
+              console.log({
+                name,
+                surname,
+                username,
+                email,
+                password,
+                dateBirth,
+                address,
+                res,
+              });
+              const addedUser = await register({
+                name,
+                surname,
+                username,
+                email,
+                password,
+                dateBirth,
+                address,
+                photo: res,
+              }).unwrap();
+              console.log("kraj");
+              toast.success(addedUser);
+              navigate(redirect);
+            } catch (error) {
+              toast.error(error.data);
+              console.log("Error:", error);
+            }
+          });
+        });
+      } catch (error) {
+        toast.error(error.data);
+      }
+    }
+  };
   return (
-    <section className="pl-[10rem] flex flex-wrap">
-      <div className="mr-[4rem] mt-[5rem]">
+    <section className="pl-[45rem] flex flex-wrap">
+      <div className="mt-[5rem] items-center">
         <h1 className="text-2xl font-semibold mb-4">Register</h1>
 
-        <form onSubmit={register} className="container w-[40rem]">
+        <form onSubmit={registUser} className="container w-[60rem]">
           <div className="my-[2rem]">
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="name" className="block text-sm font-medium">
               Name
             </label>
             <input
@@ -31,16 +89,38 @@ const Register = () => {
               id="name"
               className="mt-1 p-2 border rounded w-full"
               placeholder="Enter name"
-              value={username}
+              value={name}
               onChange={(e) => setName(e.target.value)}
             />
           </div>
-
           <div className="my-[2rem]">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="surname" className="block text-sm font-medium">
+              Surname
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter name"
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label htmlFor="username" className="block text-sm font-medium">
+              Username
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Enter name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label htmlFor="email" className="block text-sm font-medium">
               Email Address
             </label>
             <input
@@ -54,10 +134,7 @@ const Register = () => {
           </div>
 
           <div className="my-[2rem]">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="password" className="block text-sm font-medium">
               Password
             </label>
             <input
@@ -73,7 +150,7 @@ const Register = () => {
           <div className="my-[2rem]">
             <label
               htmlFor="confirmPassword"
-              className="block text-sm font-medium text-white"
+              className="block text-sm font-medium"
             >
               Confirm Password
             </label>
@@ -86,16 +163,46 @@ const Register = () => {
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
+          <div className="my-[2rem]">
+            <label htmlFor="Date birth" className="block text-sm font-medium">
+              Date birth
+            </label>
+            <DatePicker
+              selected={dateBirth}
+              onChange={(dateBirth) => setDateBirth(dateBirth)}
+              icon="fa fa-calendar"
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label htmlFor="Address" className="block text-sm font-medium">
+              Address
+            </label>
+            <input
+              type="text"
+              id="confirmPassword"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+          <div className="my-[2rem]">
+            <label htmlFor="Photo" className="block text-sm font-medium">
+              Photo
+            </label>
+            <input
+              type="file"
+              id="photo"
+              className="mt-1 p-2 border rounded w-full"
+              placeholder="Address"
+              onChange={(e) => setPhoto(e.target.files[0])}
+            />
+          </div>
 
-          {/* <button
-            //disabled={isLoading}
+          <button
             type="submit"
             className="bg-pink-500 text-white px-4 py-2 rounded cursor-pointer my-[1rem]"
-          >
-            {isLoading ? "Registering..." : "Register"}
-          </button>
-
-          {isLoading && <Loader />} */}
+          ></button>
         </form>
 
         <div className="mt-4">
